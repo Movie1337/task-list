51e774b788e7caca541b1598e99c5fa5f32b51df
Movie1337 <oldmooovie@gmail.com>
2023-04-11 15:54:35 +0300

    version v4.0

---
+++ task lists/js/script.js
@@ -0,0 +1,35 @@
+let task1 = {
+    name: 'Call Sam For payments',
+    description: 'Позвоните Сэму для оплаты',
+    dateofcreation: '11.04.2023',
+    dateofchange: '11.04.2023',
+    done: 'no',
+}
+let task2 = {
+    name: 'Make payment to Bluedart',
+    description: 'Оплатить Bluedart',
+    dateofcreation: '11.04.2023',
+    dateofchange: '11.04.2023',
+    done: 'no',
+}
+let task3 = {
+    name: 'Office rent',
+    description: 'Аренда офиса',
+    dateofcreation: '11.04.2023',
+    dateofchange: '11.04.2023',
+    done: 'no',
+}
+let task4 = {
+    name: 'Office grocery shopping',
+    description: 'Покупка продуктов в офисе',
+    dateofcreation: '11.04.2023',
+    dateofchange: '11.04.2023',
+    done: 'no',
+}
+let task5 = {
+    name: 'Ask for Lunch to Clients',
+    description: 'Попросите обед у клиентов',
+    dateofcreation: '11.04.2023',
+    dateofchange: '11.04.2023',
+    done: 'no',
+}
